a146e7892508a6acf621a69041345fb26dcbef70
kirilaf78 <kirilaf78@gmail.com>
2025-12-28 01:20:11 +0100

    higher order generics

--- ts-pure/05-generics/04-higher-order-generics.ts
+++ ts-pure/05-generics/04-higher-order-generics.ts
@@ -9,4 +9,21 @@ const numbersToStrings: Mapper<number, string> = (num) => num.toString();
 const stringArray = mapArray(numbers, numbersToStrings);
 console.log(stringArray); // Output: ['1', '2', '3', '4']
 
-interface <T, U> {
+interface IProduct{
+    id: number;
+    name: string;
+    price: number;
+}
+
+const products: IProduct[] = [
+    { id: 1, name: "Laptop", price: 1000 },
+    { id: 2, name: "Phone", price: 500 },
+    { id: 3, name: "Tablet", price: 300 },
+];
+const productNames: Mapper<IProduct, string> = (product) => product.name;
+const namesArray = mapArray(products, productNames);
+console.log(namesArray);
+
+const extractIds: Mapper<IProduct, number> = (product) => product.id;
+const idsArray = mapArray(products, extractIds);
+console.log(idsArray); // Output: ['Laptop', 'Phone', 'Tablet'] // Output: [1, 2, 3] // Output: [1, 2, 3]
